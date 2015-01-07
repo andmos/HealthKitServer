e9981c825250d3d9d8f91fe89d5b955b7edcc639
andmos <andreas.mosti@gmail.com>
2015-01-07 18:37:41 +0100

    first commit with skeleton

---
+++ iOS/Helpers/HealtKitAccess.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using MonoTouch.HealthKit;
+using MonoTouch.Foundation;
+
+namespace HealthKitServer
+{
+	public class HealtKitAccess
+	{
+		private HKHealthStore m_healthKitStore;
+
+
+		public HealtKitAccess ()
+		{
+			var distanceWalked = HKQuantityTypeIdentifierKey.DistanceWalkingRunning;
+			var tempQuantityType = HKObjectType.GetQuantityType (distanceWalked);
+
+			if (m_healthKitStore == null) 
+			{
+				HealthKitStore = new HKHealthStore (); 
+				m_healthKitStore.RequestAuthorizationToShare (new NSSet (new [] { tempQuantityType }), new NSSet (), (success, error) => {
+					Console.WriteLine ("Authorized:" + success);
+					if (error != null) {
+						Console.WriteLine ("Authorization error: " + error);
+					}
+				});
+			}
+		}
+
+		public HKHealthStore HealthKitStore
+		{ 
+			get 
+			{
+				return m_healthKitStore;
+			} 
+			private set 
+			{
+				m_healthKitStore = value; 
+			}
+		}
+	}
+		
+}
+
---
+++ iOS/Helpers/IHealthKitAccess.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace HealthKitServer
+{
+	public interface IHealthKitAccess
+	{
+		
+	}
+}
+
