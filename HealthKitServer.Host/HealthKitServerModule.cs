5f853d2d50352a12091845a2eeeb3d62436c6e3c
Andreas Mosti <andreas.mosti@gmail.com>
2015-01-08 18:58:34 +0100

    refactored. Cratet host-project

---
+++ HealthKitServer.Host/HealthKitServerModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Nancy;
+
+namespace HealthKitServer.Server
+{
+	public class HealthKitServerModule : NancyModule
+	{
+		public HealthKitServerModule() 
+		{
+
+			Get["/api/V1/totalsteps"] = parameters =>
+			{
+				var feeds = new string[] {"foo", "bar"};
+				return Response.AsJson(feeds);
+			};
+		}
+	}
+}
+
