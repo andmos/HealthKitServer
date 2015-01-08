80dd8a259ff4a9ff9315cf851c3e8d7455262b76
andmos <andreas.mosti@gmail.com>
2015-01-08 17:21:29 +0100

    Added NancyModule and selfhost

---
+++ HealthKitServer.Server/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Topshelf;
+
+namespace HealthKitServer.Server
+{
+	public class Program
+	{
+		public static void Main()
+		{
+			HostFactory.Run(x => 
+				{
+					x.UseLinuxIfAvailable();
+					x.Service<HealthKitServerSelfHost>(s => 
+						{
+							s.ConstructUsing(name => new HealthKitServerSelfHost()); 
+							s.WhenStarted(tc => tc.Start()); 
+							s.WhenStopped(tc => tc.Stop()); 
+						});
+
+					x.RunAsLocalSystem(); 
+					x.SetDescription("HealthKitServer"); 
+					x.SetDisplayName("HealthKitServer Service"); 
+					x.SetServiceName("HealthKitServer"); 
+				}); 
+		}
+	}
+}
+
---
+++ HealthKitServer.Server/Services/HealthKitServerSelfHost.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using Nancy.Hosting.Self;
+namespace HealthKitServer.Server
+{
+	public class HealthKitServerSelfHost
+	{
+
+		private NancyHost m_nancyHost;
+
+
+			public void Start()
+			{
+				m_nancyHost = new NancyHost(new System.Uri("http://localhost:5000"));
+				m_nancyHost.Start();
+
+			}
+			
+			public void Stop()
+			{
+				m_nancyHost.Stop();
+				Console.WriteLine("Stopped. Good bye!");
+			}
+	}
+}
+
