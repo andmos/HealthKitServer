17a568c3b72a1b46dccb1aa0f7217f9784b4c3f7
andmos <andreas.mosti@gmail.com>
2015-01-09 20:03:29 +0100

    Moved nacnymodule from host to server

--- HealthKitServer.Server/Services/HealthKitServerModule.cs
+++ HealthKitServer.Server/Services/HealthKitServerModule.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using Nancy;
+using Nancy.ModelBinding;
+using HealthKitServer.Server;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace HealthKitServer.Server
 {
@@ -8,10 +12,28 @@ namespace HealthKitServer.Server
 		public HealthKitServerModule() 
 		{
 
-			Get["/api/V1/totalsteps"] = parameters =>
+			Post["/api/v1/addPatient?"] = parameters =>
 			{
-				var feeds = new string[] {"foo", "bar"};
-				return Response.AsJson(feeds);
+				var person = this.Bind<Person>();
+				Container.Singleton<IHealthInfoDataStorage>().AddOrUpdatePersonHealthInfoToStorage(person);
+				return Response.AsJson(person);
+			};
+
+			Get["/api/v1/getAllPatients"] = parameters => 
+			{
+				return Response.AsJson (Container.Singleton<IHealthInfoDataStorage> ().GetAllPersons());
+			};
+
+			Get["/api/v1/getPatient"] = parameters => 
+			{
+				var id = this.Request.Query["id"];
+				int number; 
+				if(int.TryParse(id, out number))
+				{
+					return Response.AsJson(Container.Singleton<IHealthInfoDataStorage>().GetPatientHealthInfo(number));
+				}
+				return "Invalid query";
+
 			};
 		}
 	}
