fa751aee1b2875176c56b26bb94c9248b06d3aeb
Andreas Mosti <andreas.mosti@gmail.com>
2015-01-10 23:02:14 +0100

    more build erros. healthKitAccess must be async

--- HealthKitServer/ViewModels/DistanceSummaryViewModel.cs
+++ HealthKitServer/ViewModels/DistanceSummaryViewModel.cs
@@ -7,16 +7,16 @@ namespace HealthKitServer
 		private HealthKitDataDecorator m_healthKitDataDecorator; 
 		private HealthKitData m_healthKitdataObject; 
 		private IHealthKitDataUploader m_healthKitDataUploader; 
-		private string m_healthKitServerAddress;
+		private string m_healthKitServerAddress = "http://apollo.amosti.net:5002/api/v1/addPatient";
 
 		public DistanceSummaryViewModel ()
 		{
 			m_healthKitdataObject = new HealthKitData {Id = 3, DistanceReadings = new DistanceReading{}};
 			m_healthKitDataDecorator = new HealthKitDataDecorator (Container.Singleton<IHealthKitAccess> (), m_healthKitdataObject);
 			m_healthKitDataDecorator.DecorateHealthKitData ();
-	//		m_healthKitDataUploader = Container.Resolve<IHealthKitDataUploader> ();
+			m_healthKitDataUploader = Container.Resolve<IHealthKitDataUploader> ();
 
-	//		var uploaded = m_healthKitDataUploader.UploadHealthKitDataToHealthKitServer(m_healthKitServerAddress, m_healthKitdataObject);
+			var uploaded = m_healthKitDataUploader.UploadHealthKitDataToHealthKitServer(m_healthKitServerAddress, m_healthKitdataObject);
 		}
 
 		public string HealthKitServerAddress
