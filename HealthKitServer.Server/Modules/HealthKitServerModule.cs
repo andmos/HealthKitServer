eb141889a42079f7bd4ec77cb1b7ccbb0fd94300
andmos <andreas.mosti@gmail.com>
2015-01-14 22:32:52 +0100

    adde more metadata mumbo jumbo. Need view.

--- HealthKitServer.Server/Modules/HealthKitServerModule.cs
+++ HealthKitServer.Server/Modules/HealthKitServerModule.cs
@@ -3,13 +3,22 @@ using Nancy;
 using Nancy.ModelBinding;
 using HealthKitServer.Server;
 using System.Collections.Generic;
+using Nancy.Routing;
 
 namespace HealthKitServer.Server
 {
 	public class HealthKitServerModule : NancyModule
 	{
-		public HealthKitServerModule() 
+		public HealthKitServerModule(IRouteCacheProvider routeCacheProvider) 
 		{
+
+			Get["/api/v1"] = parameters =>
+			{
+				return Negotiate
+					.WithModel(routeCacheProvider.GetCache().RetrieveMetadata<HealthKitServerMetadataModule>());
+
+			};
+
 
 			Post["/api/v1/addPatient"] = parameters =>
 			{
