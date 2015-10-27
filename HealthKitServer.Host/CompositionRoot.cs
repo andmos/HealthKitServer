bdb723d3535396f6f498d1ece4702a36b476a961
andmos <andreas.mosti@gmail.com>
2015-10-27 22:08:05 +0100

    indent.

--- HealthKitServer.Host/CompositionRoot.cs
+++ HealthKitServer.Host/CompositionRoot.cs
@@ -15,29 +15,29 @@ namespace HealthKitServer.Host
 
 			switch (dataStorage) 
 			{
-			case "cache":
-				container.RegisterSingleton<IHealthKitDataStorage>(new HealthKitDataCache ()); 
-				break; 
-			case "solr":
-				var solrServerAddress = ConfigurationManager.AppSettings["SolrServerAddress"];
-				container.RegisterSingleton<IHealthKitDataStorage> (new HealthKidDataSolrConnection (solrServerAddress));
-				break;
-			case "redis":
-				var redisServerAddress = ConfigurationManager.AppSettings["RedisServerAddress"];
-				container.RegisterSingleton<IHealthKitDataStorage> (new HealthKitDataRedisConnection (redisServerAddress));
-				break;
-			case "database": 
-				if (database.ToLower().Equals("mysql")) 
-				{
-					var mysqlConnectionString = ConfigurationManager.AppSettings["ConnectionString"];
-					container.RegisterSingleton<IHealthKitDataStorage> (new HealthKitDataMysqlConnection (mysqlConnectionString));
-				}
-				if (database.ToLower().Equals("postgresql")) 
-				{
-					var postgresqlConnectionString = ConfigurationManager.AppSettings["ConnectionString"];
-					container.RegisterSingleton<IHealthKitDataStorage> (new HealthKitDataPostgresConnection (postgresqlConnectionString));
-				}
-				break;
+				case "cache":
+					container.RegisterSingleton<IHealthKitDataStorage>(new HealthKitDataCache ()); 
+					break; 
+				case "solr":
+					var solrServerAddress = ConfigurationManager.AppSettings["SolrServerAddress"];
+					container.RegisterSingleton<IHealthKitDataStorage> (new HealthKidDataSolrConnection (solrServerAddress));
+					break;
+				case "redis":
+					var redisServerAddress = ConfigurationManager.AppSettings["RedisServerAddress"];
+					container.RegisterSingleton<IHealthKitDataStorage> (new HealthKitDataRedisConnection (redisServerAddress));
+					break;
+				case "database": 
+					if (database.ToLower().Equals("mysql")) 
+					{
+						var mysqlConnectionString = ConfigurationManager.AppSettings["ConnectionString"];
+						container.RegisterSingleton<IHealthKitDataStorage> (new HealthKitDataMysqlConnection (mysqlConnectionString));
+					}
+					if (database.ToLower().Equals("postgresql")) 
+					{
+						var postgresqlConnectionString = ConfigurationManager.AppSettings["ConnectionString"];
+						container.RegisterSingleton<IHealthKitDataStorage> (new HealthKitDataPostgresConnection (postgresqlConnectionString));
+					}
+					break;
 			
 			}
 		}
